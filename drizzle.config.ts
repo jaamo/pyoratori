945cee355e72d263181f318da3b170b1b7897a9c
jaamo <jaakko@alajoki.fi>
2026-02-18 19:36:03 +0200

    Allow special chars in pw

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,17 @@ export default defineConfig({
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
-  dbCredentials: {
-    url:
-      process.env.DATABASE_URL ||
-      "postgresql://jaamo:postgres@localhost:5432/pyoratori",
-    ssl: false,
-  },
+  dbCredentials: process.env.DB_HOST
+    ? {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || 5432),
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        database: process.env.DB_NAME!,
+        ssl: false,
+      }
+    : {
+        url: "postgresql://jaamo:postgres@localhost:5432/pyoratori",
+        ssl: false,
+      },
 });
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,11 +2,20 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgresql://jaamo:postgres@localhost:5432/pyoratori",
-  ssl: false,
-});
+const pool = new Pool(
+  process.env.DB_HOST
+    ? {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || 5432),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: false,
+      }
+    : {
+        connectionString: "postgresql://jaamo:postgres@localhost:5432/pyoratori",
+        ssl: false,
+      },
+);
 
 export const db = drizzle(pool, { schema });
